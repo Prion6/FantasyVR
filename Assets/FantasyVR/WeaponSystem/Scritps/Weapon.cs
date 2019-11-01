0652ad02851244733ae079b2352e687705e923ca
san117 <santiago.cervera117@gmail.com>
2019-11-01 17:25:22 -0300

    VR Hands Improve

---
+++ Assets/FantasyVR/WeaponSystem/Scritps/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WeaponSystem
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public abstract class Weapon : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody m_rigidbody;
+        private Vector3 lastVelocity;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (lastVelocity.magnitude != 0)
+            {
+                OVRInput.SetControllerVibration(1 / lastVelocity.magnitude, Mathf.Clamp01(lastVelocity.magnitude), OVRInput.Controller.RTouch);
+            }
+        }
+    }
+}
