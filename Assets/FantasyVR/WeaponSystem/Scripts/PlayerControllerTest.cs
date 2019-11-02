a020dd517b98e663ed3d080b68c0b7806a22223b
Claudio Rubio <43611630+ClaudixoPG@users.noreply.github.com>
2019-11-02 19:43:13 -0300

    WeaponTrigger

---
+++ Assets/FantasyVR/WeaponSystem/Scripts/PlayerControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerTest : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    private CharacterController player;
+
+    public float playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+    }
+    private void FixedUpdate()
+    {   
+        player.Move(new Vector3(horizontalMove,0,verticalMove)* playerSpeed);
+    }
+}
---
+++ Assets/FantasyVR/WeaponSystem/Scripts/WeaponDetection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDetection : MonoBehaviour
+{
+    public enum Direction { Up, Down, Left, Right, Up_Left, Up_Right, Down_Left, Down_Right };
+    public GameObject LeftHand;
+    public GameObject RightHand;
+    public GameObject player;
+    public Direction dir;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (LeftHand.GetComponent<Collider>() == other )
+        {
+            Debug.Log("Mano Izquierda en " + dir);
+        }
+        if (RightHand.GetComponent<Collider>() == other)
+        {
+            Debug.Log("Mano Derecha en " + dir);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (LeftHand.GetComponent<Collider>() == other ||
+            (player.GetComponent<Collider>() == other && Input.GetKey(KeyCode.A)))
+        {
+            Debug.Log("Mano Izquierda en " + dir);
+        }
+        if (RightHand.GetComponent<Collider>() == other ||
+            (player.GetComponent<Collider>() == other && Input.GetKey(KeyCode.D)))
+        {
+            Debug.Log("Mano Derecha en " + dir);
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("Direccion" + dir);
+        Debug.Log("sali del collider");
+    
+    }
+
+}
