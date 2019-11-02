0733e3093835647f3b5b0ed9f34f0769a1b05680
san117 <santiago.cervera117@gmail.com>
2019-11-02 19:39:10 -0300

    [Ragdoll]

---
+++ Assets/FantasyVR/EnemySystem/Scripts/BodyPart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BodyPart : MonoBehaviour
+{
+    public LayerMask collidableObjects;
+
+    public UnityEvent onCollisionEnter;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((collidableObjects.value & (1 << collision.collider.gameObject.layer)) > 0)
+        {
+            print(collision.collider.name);
+            onCollisionEnter.Invoke();
+        }
+    }
+}
