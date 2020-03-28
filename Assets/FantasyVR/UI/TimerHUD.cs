c340ac153ce2f37cd035ab3ab4f31acd62f9c61f
Gabriel Sepulveda <gsepulveda17@alumnos.utalca.cl>
2020-03-28 13:43:38 -0300

    Timer Implemented

---
+++ Assets/FantasyVR/UI/TimerHUD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerHUD : MonoBehaviour
+{
+    public Text text;
+    public Timer timer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        timer.OnInit += Actualize;
+        timer.OnRunning += Actualize;
+    }
+
+    void Start()
+    {
+        timer.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Actualize()
+    {
+        Debug.Log("Timer Actualize");
+        text.text = (timer.ContdownMin() + ":" + timer.ContdownSec() + ":" + timer.ContodownMillis());
+    }
+}
