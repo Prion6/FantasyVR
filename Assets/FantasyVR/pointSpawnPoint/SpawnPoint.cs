84753c0adbce4f1a429c8f4e7b9db1f46401d239
Nicolas Romero <nare.rare.fire@gmail.com>
2020-03-28 17:51:01 -0300

    new scene

---
+++ Assets/FantasyVR/pointSpawnPoint/SpawnPoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject toSpawn;
+
+    private GameObject spawned;
+
+    public float deltaTimer = 10;
+    private float timer = 0;
+
+    private void Awake()
+    {
+        //var refernece = transform.GetChild(0);
+        //Destroy(refernece.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawned == null)
+        {
+            if(timer >= deltaTimer)
+            {
+                timer = 0;
+                spawned = Instantiate(toSpawn, transform.position, transform.rotation,transform);
+            }
+            timer += Time.deltaTime;
+        }
+    }
+}
