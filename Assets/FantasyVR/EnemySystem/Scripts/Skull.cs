59369ba4b070f952edd2b02c548f686ed1f53323
Gabriel Sepulveda <gsepulveda17@alumnos.utalca.cl>
2020-03-29 15:46:47 -0300

    Skull

---
+++ Assets/FantasyVR/EnemySystem/Scripts/Skull.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Skull : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rb;
+    public float movForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<OVRPlayerController>().transform;
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        rb.AddForce(movForce*Vector3.forward);
+    }
+}
